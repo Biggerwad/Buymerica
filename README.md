7b104668e171c1e4cc245e0e14c12ce03b78cf8c
Daniel Wesey <weseydaniel74@gmail.com>
2023-09-06 18:02:22 +0100

    head update

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ E-commerce Web application built woth React.js
 Features:
 Search Items and Add to cart
 Check your account
+Light and Dark Mode
 
 Buymerica!
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { render } from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './GlobalComponents/ThemeProvider';
 import { CartProvider } from 'react-use-cart';
 
@@ -16,9 +14,3 @@ render(
     </ThemeProvider>
   </React.StrictMode>
 , document.getElementById('root'));
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
